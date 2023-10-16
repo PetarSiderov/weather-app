73ea10ebba0285de7eb2ca0079c04bb498ddf79a
Petar <Petar@WINDOWS-MUNF8E1>
2023-10-16 23:23:39 +0200

    added route for the login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './authentication/auth.guard';
 import { WeatherComponent } from './weather/weather.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent,  canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent,  canActivate: [AuthGuard] },
   { path: 'sign-up', component: SignUpComponent, canActivate: [AuthGuard] },
   { path: 'weather', component: WeatherComponent,canActivate: [AuthGuard] }
